e65d4c3d573e5873f9073233ef8cfef4f25e97d9
Spencer Heffley <spheffley@yahoo.com>
2022-11-21 18:09:14 -0500

    Moved the <li> tag to the NavBarLink component to clean up the code in the NavBar component

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -4,21 +4,11 @@ const NavBar = () => {
     return (
         <nav className="bg-gray-100 p-2">
             <ul className="flex justify-center">
-                <li>
-                    <NavBarLink href="/">About Us</NavBarLink>
-                </li>
-                <li>
-                    <NavBarLink href="/about">Sermons</NavBarLink>
-                </li>
-                <li>
-                    <NavBarLink href="/events">Contact</NavBarLink>
-                </li>
-                <li>
-                    <NavBarLink href="/sermons">Online Giving</NavBarLink>
-                </li>
-                <li>
-                    <NavBarLink href="/contact">Helping Hands Daycare</NavBarLink>
-                </li>
+                <NavBarLink href="/">About Us</NavBarLink>  
+                <NavBarLink href="/about">Sermons</NavBarLink>      
+                <NavBarLink href="/events">Contact</NavBarLink> 
+                <NavBarLink href="/sermons">Online Giving</NavBarLink>       
+                <NavBarLink href="/contact">Helping Hands Daycare</NavBarLink>            
             </ul>
         </nav>
     )
--- src/components/UI/NavBarLink.js
+++ src/components/UI/NavBarLink.js
@@ -1,6 +1,8 @@
 const NavBarLink = props => {
     return (
-        <a href={props.href} className="ml-10 text-black text-sm font-bold hover:text-gray-500"> {props.children} </a>
+        <li className="ml-10">
+            <a href={props.href} className="text-black text-sm font-bold hover:text-gray-500"> {props.children} </a>
+        </li>
     )
 }
 
